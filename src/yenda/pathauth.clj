94622acf327ccb400394acaecddbc219d2028e61
yenda <yenda1@gmail.com>
2023-01-27 17:06:36 +0100

    First iteration

--- src/yenda/pathauth.clj
+++ src/yenda/pathauth.clj
@@ -1,30 +1,64 @@
 (ns yenda.pathauth
-  (:require [com.wsscode.pathom3.connect.operation :as pco]))
+  (:require [com.wsscode.pathom3.connect.operation :as pco]
+            [com.wsscode.pathom3.format.shape-descriptor :as pfsd]
+            [edn-query-language.core :as eql]
+            [clojure.set :as set]))
 
 (defn circular-dependency? [resolver]
-  true)
+  (-> resolver :config ::circular?))
 
 (defn wrap-authorization [resolver]
-  (if-let [authorizations (:authorization resolver)]
+  (if-let [authorizations (-> resolver :config ::auth)]
     (if-not (circular-dependency? resolver)
       ;; simple resolver
-      (update resolver ::pco/input concat authorizations)
+      (let [input (get-in resolver [:config ::pco/input])
+            new-input (-> input (concat authorizations) vec)
+            new-requires (pco/describe-input new-input)]
+        (-> resolver
+            (assoc-in [:config ::pco/input] new-input)
+            (update :config merge  new-requires)))
 
       ;; special resolver
-      (let [{::pco/keys [input output op-name]
-             ::keys [authorizations]} resolver
+      (let [{:keys [resolve config]} resolver
+            {::pco/keys [input output op-name]} config
             pending-resolver-op-name (symbol
                                       "pending-authorization"
                                       (str (namespace op-name) "--" (name op-name)))
             pending-key (keyword pending-resolver-op-name)]
-        [resolver
+        [(pco/resolver
+          {::pco/op-name op-name
+           ;; TODO: optimize to only ouput the inputs that are required by the
+           ;; pending resolver
+           ::pco/output [{pending-key (vec (into #{} (concat input output)))}]
+           ::pco/input input
+           ::pco/batch? true
+           ::pco/resolve (fn [env inputs]
+                           (mapv (fn [result input]
+                                   {pending-key (merge input result)})
+                                 (resolve env inputs)
+                                 inputs))})
          (pco/resolver
-          (cond-> {::pco/op-name pending-resolver-op-name
-                   ::pco/output  output
-                   ::pco/batch?  true
-                   ::pco/resolve (fn [env inputs]
-                                   (mapv pending-key inputs))
-                   ::pco/input [{pending-key (concat output authorizations)}]}))]))
+          {::pco/op-name pending-resolver-op-name
+           ::pco/output  output
+           ::pco/batch?  true
+           ::pco/resolve (fn [env inputs]
+                           (mapv pending-key inputs))
+           ::pco/input [{pending-key (vec (into #{} (concat input output authorizations)))}]})]))
 
     ;; resolver without authorization or authorization resolver
     resolver))
+
+(defn auth-setup [operation-or-operations]
+  ;; TODO: automatically detect circular dependencies between authorization resolvers
+  ;; and resolvers
+  #_(let [auth-attributes (reduce (fn [acc {:keys [config]}]
+                                    (if-let [authorizations (::auth config)]
+                                      (set/union acc (into #{} authorizations))
+                                      acc))
+                                  #{}
+                                  resolvers)])
+  (mapv (fn [operation-or-operations]
+          (if (sequential? operation-or-operations)
+            (auth-setup operation-or-operations)
+            (wrap-authorization operation-or-operations)))
+        operation-or-operations))
--- test/yenda/pathauth_test.clj
+++ test/yenda/pathauth_test.clj
@@ -1,7 +1,93 @@
 (ns yenda.pathauth-test
   (:require [clojure.test :refer :all]
-            [yenda.pathauth :refer :all]))
+            [yenda.pathauth :refer :all :as pathauth]
+            [com.wsscode.pathom3.format.shape-descriptor :as pfsd]
+            [com.wsscode.pathom3.connect.operation :as pco]
+            [com.wsscode.pathom3.connect.indexes :as pci]
+            [com.wsscode.pathom3.connect.planner :as pcp]
+            [edn-query-language.core :as eql]
+            [com.wsscode.pathom3.interface.eql :as p.eql]
+            [com.wsscode.pathom3.interface.async.eql :as p.a.eql]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(pco/defresolver toy
+  [env input]
+  {::pco/input  [:toy/id]
+   ::pco/batch? true
+   ::pco/output [:toy/name :toy/id :toy/order]}
+
+  (map #(get {1 {:toy/name "Bobby" :toy/id 1 :toy/order 3}
+              2 {:toy/name "Alice" :toy/id 2 :toy/order 2}
+              3 {:toy/name "Rene" :toy/id 3 :toy/order 1}}
+             (:toy/id %))
+       input))
+
+(pco/defresolver child-toys
+  [env input]
+  {::pco/input  [:child/id]
+   ::pco/batch? true
+   ::pathauth/auth [:child/parent?]
+   ::pco/output [{:child/toys [:toy/id]}]}
+
+  [{:child/toys [{:toy/id 1}
+                 {:toy/id 2}
+                 {:toy/id 3}]}])
+
+(pco/defresolver favorite-toy
+  [env input]
+  {::pco/input  [{:child/toys [:toy/id :toy/order]}]
+   ::pco/batch? true
+   ::pco/output [{:child/favorite-toy [:toy/id]}]}
+
+  [{:child/favorite-toy (->> (:child/toys (first input))
+                             (sort-by :toy/order)
+                             first)}])
+
+(pco/defresolver child-parents
+  [env inputs]
+  {::pco/input  [:child/id]
+   ::pco/batch? true
+   ::pathauth/auth [:child/parent?]
+   ::pathauth/circular? true
+   ::pco/output [:parent/id]}
+  (let [parents {1  4
+                 2  5
+                 3  6}]
+    (mapv (fn [input]
+            {:parent/id (get parents (:child/id input))})
+          inputs)))
+
+(pco/defresolver parent?
+  [{:keys [parent-id] :as env} inputs]
+  {::pco/input  [:child/id :parent/id]
+   ::pco/batch? true
+   ::pco/output [:child/parent?]}
+  (mapv (fn [input]
+          (when (= parent-id (:parent/id input))
+            {:child/parent? true}))
+        inputs))
+
+
+(def env (-> (pci/register
+              (pathauth/setup
+               [toy
+                child-toys
+                favorite-toy
+                child-parents
+                parent?]))))
+
+(comment
+  ;; authorized, will return
+  (let [query [{[:child/id 1] [{:child/toys [:toy/name]}]}]]
+    @(p.a.eql/process (assoc env
+                             ::p.a.eql/parallel? true
+                             :parent-id 4) query))
+  ;; => {[:child/id 1] #:child{:toys [#:toy{:name "Bobby"} #:toy{:name "Alice"} #:toy{:name "Rene"}]}}
+
+  ;; unauthorized, will fail
+  (let [query [{[:child/id 1] [{:child/toys [:toy/name]}]}]]
+    @(p.a.eql/process (assoc env
+                             ::p.a.eql/parallel? true
+                             ) query))
+
+
+  )
