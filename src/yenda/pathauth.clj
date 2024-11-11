c787938930134722aed7c9fbb0279dae31c833e0
Eric Dvorsak <yenda1@gmail.com>
2024-11-11 03:08:31 +0100

    fix: support more than one auth resolver

--- src/yenda/pathauth.clj
+++ src/yenda/pathauth.clj
@@ -1,49 +1,51 @@
 (ns yenda.pathauth
-  (:require [com.wsscode.pathom3.connect.operation :as pco]
-            [com.wsscode.pathom3.format.shape-descriptor :as pfsd]
-            [edn-query-language.core :as eql]
-            [clojure.set :as set]
-            [taoensso.timbre :as log]))
+  (:require
+   [com.fulcrologic.rad.attributes :as-alias attr]
+   [edn-query-language.core :as eql]
+   [taoensso.timbre :as log]))
 
 (def auth ::auth)
+(def restricted ::restricted)
 
 (defn auth-attributes [attributes schema]
-  (let [k->attr (taoensso.encore/keys-by :com.fulcrologic.rad.attributes/qualified-key attributes)]
+  (let [k->attr (taoensso.encore/keys-by ::attr/qualified-key attributes)]
     (->> attributes
-         (filter #(= schema (:com.fulcrologic.rad.attributes/schema %)))
+         ;; are we sure we even want to filter by schema here?
+         #_(filter #(= schema (::attr/schema %)))
          (reduce
-          (fn [acc attribute]
-            (assoc acc (:com.fulcrologic.rad.attributes/qualified-key attribute)
-                   (if (:com.fulcrologic.rad.attributes/identity? attribute)
-                     (::auth attribute)
+          (fn [auth-attributes {::attr/keys [qualified-key identity? identities]
+                                ::keys [auth] :as attribute}]
+            (assoc auth-attributes qualified-key
+                   (if identity?
+                     auth
                      (vec (mapcat (fn [entity-id]
                                     (::auth (k->attr entity-id)))
-                                  (:com.fulcrologic.rad.attributes/identities attribute))))))))))
+                                  identities)))))
+          {}))))
 
-
-(defn safe-query
-  "remove the pending-authorization keys from the query"
-  [query]
-  (let [ast (eql/query->ast query)]
-    (eql/transduce-children (map (fn [k]
-                                   (when (= "pending-authorization" (namespace (:dispatch-key k)))
-                                     (throw (Exception. "Illegal query")))
-                                   k))
-                            ast)))
-
+(defn children-auth-attributes [auth-attrs children]
+  (reduce (fn [acc {:keys [dispatch-key]}]
+            (into acc (get auth-attrs dispatch-key)))
+          #{}
+          children))
 
 (defn auth-query
   "remove the pending-authorization keys from the query"
-  [auth-attributes query]
+  [auth-attrs query]
   (let [ast (eql/query->ast query)
-        authed-query (-> (eql/transduce-children (map (fn [k]
-                                                        (if-let [authorizations (some #(get auth-attributes (:dispatch-key %)) (:children k))]
-                                                          (reduce (fn [acc authorization]
-                                                                    (update acc :children conj {:type :prop :dispatch-key authorization :key authorization}))
-                                                                  k
-                                                                  authorizations)
-                                                          k)))
-                                                 ast)
-                         eql/ast->query)]
+        authed-query
+        (-> (eql/transduce-children
+             (map (fn [{:keys [children] :as ast-node}]
+                    (reduce (fn [acc authorization]
+                              (update acc
+                                      :children conj
+                                      {:type :prop
+                                       :dispatch-key authorization
+                                       :key authorization}))
+                            ast-node
+                            (children-auth-attributes auth-attrs children))))
+             ast)
+            eql/ast->query)]
     (log/debug :authed-query (pr-str authed-query))
     (eql/query->ast authed-query)))
+
--- src/yenda/pathauth.cljs
+++ src/yenda/pathauth.cljs
@@ -1,3 +1,4 @@
 (ns yenda.pathauth)
 
 (def auth ::auth)
+(def restricted ::restricted)
